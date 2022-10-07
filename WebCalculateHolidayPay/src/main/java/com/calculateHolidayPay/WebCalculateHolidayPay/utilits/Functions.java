f693c7a679d0fb3e166b69df8b69824c6683f83b
Ivan Bakaev <100419665+Ivan12121@users.noreply.github.com>
2022-10-07 19:57:37 +0400

    WebCalculateHolidaypay
    WebCalculateHolidaypay

--- WebCalculateHolidayPay/src/main/java/com/calculateHolidayPay/WebCalculateHolidayPay/utilits/Functions.java
+++ WebCalculateHolidayPay/src/main/java/com/calculateHolidayPay/WebCalculateHolidayPay/utilits/Functions.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -16,51 +17,52 @@ public class Functions {
     @Autowired
     WeekendsDays weekendsDays;
 
-    public String vacationPayCalculation(String startDay, String endDay, Double salary, Model model) {
-        try {
-            String[] splitStartDay = startDay.split(",");
-            String[] splitEndDay = endDay.split(",");
+    public String vacationPayCalculation(String startDay, String endDay, Double salary, Model model) throws ParseException {
+        Date startDayHoliday = new SimpleDateFormat("yyyy-MM-dd").parse(startDay);
+        Date endDayHoliday = new SimpleDateFormat("yyyy-MM-dd").parse(endDay);
 
-            Date startDayHoliday = new SimpleDateFormat("yyyy-MM-dd").parse(splitStartDay[1]);
-            Date endDayHoliday = new SimpleDateFormat("yyyy-MM-dd").parse(splitEndDay[1]);
+        int dayComparisonResult = startDayHoliday.compareTo(endDayHoliday);
+        if (dayComparisonResult > 0) {
+            model.addAttribute("test", "Vacation start date cannot be later than end date");
+        }
+        else if(dayComparisonResult == 0) {
+            model.addAttribute("test", "Start and end of vacation cannot be equal");
+        }
+        else {
+            long startDayHolidayInMilli = startDayHoliday.getTime();
+            long endDayHolidayInMilli = endDayHoliday.getTime();
+            long amountOfVacationDaysInMilli = endDayHolidayInMilli - startDayHolidayInMilli;
+            long amountOfVacationDays = amountOfVacationDaysInMilli / 1000 / 60 / 60 / 24;
 
-            int result = startDayHoliday.compareTo(endDayHoliday);
-            if (result > 0) {
-                model.addAttribute("test", "Vacation start date cannot be later than end date");
+            if (amountOfVacationDays > 28) {
+                model.addAttribute("test", "Maximum 28 vacation days");
             } else {
-                long startDayHolidayInMilli = startDayHoliday.getTime();
-                long endDayHolidayInMilli = endDayHoliday.getTime();
-                long amountOfVacationDaysInMilli = endDayHolidayInMilli - startDayHolidayInMilli;
-                long amountOfVacationDays = amountOfVacationDaysInMilli / 1000 / 60 / 60 / 24;
+                LocalDate startDayHolidayToLocalDate = Instant.ofEpochMilli(startDayHolidayInMilli).atZone(ZoneId.systemDefault()).toLocalDate();
+                LocalDate endDayHolidayToLocalDate = Instant.ofEpochMilli(endDayHolidayInMilli).atZone(ZoneId.systemDefault()).toLocalDate();
 
-                if (amountOfVacationDays > 28) {
-                    model.addAttribute("test", "Maximum 28 vacation days");
-                } else {
-                    LocalDate startDayHolidayToLocalDate = Instant.ofEpochMilli(startDayHolidayInMilli).atZone(ZoneId.systemDefault()).toLocalDate();
-                    LocalDate endDayHolidayToLocalDate = Instant.ofEpochMilli(endDayHolidayInMilli).atZone(ZoneId.systemDefault()).toLocalDate();
-
-                    for (LocalDate ld : startDayHolidayToLocalDate.datesUntil(endDayHolidayToLocalDate).collect(Collectors.toList())) {
-                        String DayAndMonth = ld.getDayOfMonth() + "." + ld.getMonthValue();
-                        if (DayAndMonth.equals(weekendsDays.nativity)) {
-                            amountOfVacationDays++;
-                        } else if (DayAndMonth.equals(weekendsDays.russiaDay)) {
-                            amountOfVacationDays++;
-                        } else if (DayAndMonth.equals(weekendsDays.victoryDay)) {
-                            amountOfVacationDays++;
-                        }
-                    }
-
-                    if (salary <= 0) {
-                        model.addAttribute("test", "Salary must be greater than zero");
-                    } else {
-                        double vacationPay = ((salary * 12) / 351.6) * amountOfVacationDays;
-                        model.addAttribute("test", "Vacation pay: " + Math.ceil(vacationPay));
+                for (LocalDate ld : startDayHolidayToLocalDate.datesUntil(endDayHolidayToLocalDate).collect(Collectors.toList())) {
+                    String DayAndMonth = ld.getDayOfMonth() + "." + ld.getMonthValue();
+                    if (DayAndMonth.equals(weekendsDays.nativity)) {
+                        amountOfVacationDays++;
+                    } else if (DayAndMonth.equals(weekendsDays.russiaDay)) {
+                        amountOfVacationDays++;
+                    } else if (DayAndMonth.equals(weekendsDays.victoryDay)) {
+                        amountOfVacationDays++;
                     }
                 }
+
+                if (salary <= 0) {
+                    model.addAttribute("test", "Salary must be greater than zero");
+                } else {
+                    model.addAttribute("test", "Vacation pay: " + getVacationPayCost(salary, amountOfVacationDays));
+                }
             }
-        } catch (Exception e) {
-            System.out.println(e);
         }
         return "calculate";
+    }
+
+    public double getVacationPayCost(Double salary, long amountOfVacationDays) {
+        double vacationPay = ((salary * 12) / 351.6) * amountOfVacationDays;
+        return Math.ceil(vacationPay);
     }
 }
